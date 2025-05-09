d89c62021759f6c4db41f4b2faff79cbf2a8fd4b
Masaki <ms-furukawa@unext-hd.jp>
2025-05-09 15:19:28 +0900

    タグの設定を必須とした

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -5,9 +5,10 @@ class TaskForm(forms.Form):
     title = forms.CharField(max_length=100, label='タイトル')
     description = forms.CharField(widget=forms.Textarea, required=False, label='内容')
     due_date = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}), required=False, label='締め切り')
+
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
         widget=forms.CheckboxSelectMultiple,
-        required=False,
+        required=True,
         label='タグ'
     )
